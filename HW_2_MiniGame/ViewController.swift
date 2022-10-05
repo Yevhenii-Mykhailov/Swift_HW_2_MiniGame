7057c7743e5194203fd79c6ff40c9f3258e18fa0
Yevhenii Mykhailovskyi <mihaylovskiyeugeny@gmail.com>
2022-10-05 20:12:36 +0300

    Initial Commit

---
+++ HW_2_MiniGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2_MiniGame
+//
+//  Created by Yevhenii M on 05.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
